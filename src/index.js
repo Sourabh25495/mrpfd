7eb9d278d0f6f295cea73c0f5fdce0fd0d332436
Sourabh Kulkarni <sourabhuk254@gmail.com>
2020-11-11 12:46:50 -0500

    initial setup for application

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 const users = require('./../data/users.json');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('../data/users.json');
+
+const app = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+console.log("DATA", db);
+
+app.listen(port, () => {
+  console.log("We are live on " + port);
+})
