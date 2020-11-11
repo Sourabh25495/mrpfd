baeb828cde64da7de166b7d4469037ac0226f014
Sourabh Kulkarni <sourabhuk254@gmail.com>
2020-11-11 16:00:41 -0500

    update user information

--- routes/endpoints/UserOperations.js
+++ routes/endpoints/UserOperations.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {getAllUsers} = require('./UserOperations/GetUserList');
 const {addNewUser} = require('./UserOperations/AddNewUser');
+const {updateUser} = require('./UserOperations/UpdateUser')
 const {deleteUserByEmail} = require('./UserOperations/DeleteUser');
 
 router.get('/get-user-list', (req, res) => {
@@ -14,5 +15,9 @@ router.use('/delete-user/:email', (req, res) => {
 
 router.post('/add-new-user', (req, res) => {
   addNewUser(req, res);
+})
+
+router.put('/update-user', (req, res) => {
+  updateUser(req, res);
 })
 module.exports = router;
