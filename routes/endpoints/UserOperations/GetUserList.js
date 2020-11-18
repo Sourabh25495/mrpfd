539811117238db2b6a626aa24c07eb689ef9f4d3
Sourabh Kulkarni <sourabhuk254@gmail.com>
2020-11-17 21:58:55 -0500

    Sorting endpoint feature

--- routes/endpoints/UserOperations/GetUserList.js
+++ routes/endpoints/UserOperations/GetUserList.js
@@ -13,7 +13,6 @@ const chunkArray = (inputArray, chunk_size) => {
   
   for (index = 0; index < Number(arrayLength); index = +index + +chunk_size) {
     myChunk = inputArray.slice(index, +index + +chunk_size);
-    // Do something if you want with the group
     groupedArray.push(myChunk);
   }
   
@@ -23,33 +22,56 @@ const chunkArray = (inputArray, chunk_size) => {
 const getUserListChunk = (req, res) => {
   const existUsers = getUserData();
   const {page, limit} = req.query;
-  
-  const group = chunkArray(existUsers, limit);
-  
-  if(group.length < page) {
-    res.status(200).json({
-      status: 'Success',
-      message: 'No records for this page.'
-    })
+  if (!Number.isInteger(page) && !Number.isInteger(limit)) {
+    return res.status(400).send({error: true, msg: 'Bad Request! Invalid Input!'});
   } else {
-    res.send(group[page - 1]);
+    const group = chunkArray(existUsers, limit);
+    
+    if (group.length < page) {
+      res.status(200).json({
+        status: 'Success',
+        message: 'No records for this page.'
+      })
+    } else {
+      res.send(group[page - 1]);
+    }
   }
   
+}
+
+const getAscendingSortOrder = (prop) => {
+  return function(a, b) {
+    if (a[prop] > b[prop]) {
+      return 1;
+    } else if (a[prop] < b[prop]) {
+      return -1;
+    }
+    return 0;
+  }
+}
+
+const getDescendingSortOrder = (prop) => {
+  return function(a, b) {
+    if (a[prop] < b[prop]) {
+      return 1;
+    } else if (a[prop] > b[prop]) {
+      return -1;
+    }
+    return 0;
+  }
 }
 
 const getUserListBySort = (req, res) => {
   const existUsers = getUserData();
   const {sortBy, sortDirection} = req.query;
   
-  console.log("sort", sortBy, sortDirection)
-  if (sortDirection === 'ascending') {
-    res.send(existUsers.sort((a, b) => {
-      console.log(a[sortBy]);
-      a[sortBy] - b[sortBy]
-    }));
+  if (sortDirection === 'descending') {
+    const sorted = existUsers.sort(getDescendingSortOrder(sortBy));
+    res.send(sorted);
+  } else {
+    const sorted = existUsers.sort(getAscendingSortOrder(sortBy));
+    res.send(sorted);
   }
-  
-  
 }
 
 module.exports = {
